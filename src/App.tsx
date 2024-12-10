bdd704d4d8f98dc9f61b8f35ff1249a0a484639b
Ricardo Ortega Pacheco <116306561+ricardoortega97@users.noreply.github.com>
2024-12-10 03:40:26 +0000

    Bug 5 fixed

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,10 @@ export function App() {
     transactionsByEmployeeUtils.invalidateData();
 
     await employeeUtils.fetchAll();
+    setIsLoading(false)
     await paginatedTransactionsUtils.fetchAll();
 
-    setIsLoading(false);
+    // setIsLoading(false);
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils]);
 
   const loadTransactionsByEmployee = useCallback(
